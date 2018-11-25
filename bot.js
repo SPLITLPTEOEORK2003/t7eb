0c274f23bcc08e0a70ccae442c49eaa6d7a47fe4
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-25 16:29:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-      if(member.guild.id !== '502101825806139412') return;
+      if(member.guild.id !== '497101650037702669') return;
     setTimeout(function() {
-    member.guild.channels.find(r => r.id === '497101650037702669').send('**Welcome To Black Pink  :839b5b349bd15614b3ffc589da484578: **: ,');
+    member.guild.channels.find(r => r.id === '502101825806139412').send('**Welcome To Black Pink  :839b5b349bd15614b3ffc589da484578: **: ,');
 },3000);
 });
 
